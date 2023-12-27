ab2fc3646c6932d2920a1a47fba891f82d98f276
Ashera Cordova <ashera.cordova@gmail.com>
2023-12-27 19:05:54 +0530

    Added support for animaion.

--- browser/src/main/java/com/ashera/absolutelayout/AbsoluteLayoutImpl.java
+++ browser/src/main/java/com/ashera/absolutelayout/AbsoluteLayoutImpl.java
@@ -355,6 +355,26 @@ return layoutParams.y;			}
 		}
 		@Override
 		public void setMyAttribute(String name, Object value) {
+			if (name.equals("state0")) {
+				setState0(value);
+				return;
+			}
+			if (name.equals("state1")) {
+				setState1(value);
+				return;
+			}
+			if (name.equals("state2")) {
+				setState2(value);
+				return;
+			}
+			if (name.equals("state3")) {
+				setState3(value);
+				return;
+			}
+			if (name.equals("state4")) {
+				setState4(value);
+				return;
+			}
 			AbsoluteLayoutImpl.this.setAttribute(name, value, true);
 		}
         @Override
@@ -364,6 +384,21 @@ return layoutParams.y;			}
             
         }
         
+    	public void setState0(Object value) {
+    		ViewImpl.setState(AbsoluteLayoutImpl.this, 0, value);
+    	}
+    	public void setState1(Object value) {
+    		ViewImpl.setState(AbsoluteLayoutImpl.this, 1, value);
+    	}
+    	public void setState2(Object value) {
+    		ViewImpl.setState(AbsoluteLayoutImpl.this, 2, value);
+    	}
+    	public void setState3(Object value) {
+    		ViewImpl.setState(AbsoluteLayoutImpl.this, 3, value);
+    	}
+    	public void setState4(Object value) {
+    		ViewImpl.setState(AbsoluteLayoutImpl.this, 4, value);
+    	}
         	public void state0() {
         		ViewImpl.state(AbsoluteLayoutImpl.this, 0);
         	}
